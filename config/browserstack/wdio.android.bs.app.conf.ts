a58132562748f21dd5573e90c7392cd806af5b16
Scottie Crump <sncrump29@gmail.com>
2021-11-18 21:00:03 -0500

    feat: run native app tests on browserStack

--- config/browserstack/wdio.android.bs.app.conf.ts
+++ config/browserstack/wdio.android.bs.app.conf.ts
@@ -3,34 +3,21 @@ import { config } from '../wdio.shared.conf'
 // ============
 // Specs
 // ============
-config.specs = ['./test/specs/**/*.spec.js']
+config.specs = ['./test/specs/**/*.spec.ts']
 config.exclude = []
 
 // =============================
 // Browserstack specific config
 // =============================
 // User configuration
-config.user = process.env.BROWSERSTACK_USER || 'BROWSERSTACK_USER'
+config.user = process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME'
 config.key = process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY'
-// Use browserstack service
-config.services = ['browserstack']
 
-// ============
-// Capabilities
-// ============
-// For all capabilities please check
-// http://appium.io/docs/en/writing-running-appium/caps/#general-capabilities
 config.capabilities = [
   {
-    // Set your BrowserStack config
-
-    // Set URL of the application under test
+    device: 'Samsung Galaxy S21',
+    os_version: '11.0',
     app: process.env.BROWSERSTACK_APP_ID || 'BROWSERSTACK_APP_ID',
-
-    // Specify device and os_version for testing
-    device: 'Google Pixel 3',
-    os_version: '9.0',
   },
 ]
-
 exports.config = config
